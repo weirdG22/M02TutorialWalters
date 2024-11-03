8a504a8b545d92934298891ec5ccbba7ed8b81c3
Eli Walters <43915628+weirdG22@users.noreply.github.com>
2024-11-03 18:30:00 +0000

    Final Product

---
+++ script.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2VpcmRnIiwiYSI6ImNsdGYzYW4zcjA2a2kyaXJ6MXdsaHp2NTMifQ.AvphYO2ZUkmUcNHB1DK-uA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-71.060982, 42.35725]
+});
